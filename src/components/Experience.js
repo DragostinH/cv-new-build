38f84b8e356561e4dd30d9be7adff20b4bcbfd8f
DragostinH <dragostin.hristov@gmail.com>
2022-05-15 23:14:03 +0300

    added continue button functionality on add experience screen

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,7 +6,7 @@ import "../styles/Experience.scss"
 export default function Experience() {
     let experienceViewToggle;
     let addExperienceWindow;
-    let listOrTextSwitch;
+    // let listOrTextSwitch;
 
     const [state, setState] = useState({
         input: false,
@@ -61,6 +61,11 @@ export default function Experience() {
         responsibilities: [],
         currentJob: false
 
+    });
+
+    const [styleData, setStyle] = useState({
+        jobInfoLeft: "0%",
+        jobResponsbLeft: "100%",
     })
 
 
@@ -95,6 +100,12 @@ export default function Experience() {
             ...state,
             addExp: false,
         })
+
+        setStyle({
+            ...styleData,
+            jobInfoLeft: "0%",
+            jobResponsbLeft: "100%",
+        })
     }
 
     const handleOnChange = (e) => {
@@ -126,18 +137,18 @@ export default function Experience() {
     //     })
     // }
 
-    function toggleBetweenListOrText(state, listOrTextSwitch) {
-        state.textOrList ?
-            listOrTextSwitch =
-            <textarea name="responsb" id="responsb" cols="30" rows="10">
+    // function toggleBetweenListOrText(state, listOrTextSwitch) {
+    //     state.textOrList ?
+    //         listOrTextSwitch =
+    //         <textarea name="responsb" id="responsb" cols="30" rows="10">
 
-            </textarea> :
-            listOrTextSwitch =
-            <label htmlFor="">
-                <input type="text" name="" id="" />
-            </label>;
-        return listOrTextSwitch;
-    }
+    //         </textarea> :
+    //         listOrTextSwitch =
+    //         <label htmlFor="">
+    //             <input type="text" name="" id="" />
+    //         </label>;
+    //     return listOrTextSwitch;
+    // }
 
 
     const showEditPage = () => {
@@ -147,21 +158,36 @@ export default function Experience() {
         })
     }
 
-    const textAreaOnChange = (e) => {
-
-        console.log(e.target.value.split(/\r?\n/));
-    }
-
-    const areaSelected = (e) => {
-        alert(window.getSelection())
-    }
-
-    const handleChecked = (e) => {
-        setState({
-            ...state,
-            textOrList: e.target.checked,
+    const handleContinue = (e) => {
+        setStyle({
+            ...styleData,
+            jobInfoLeft: "-100%",
+            jobResponsbLeft: "0%",
         })
     }
+
+    const testStyle = (e) => {
+        setStyle({
+            ...styleData,
+            [e.target.id]: 100,
+        })
+    }
+
+    // const textAreaOnChange = (e) => {
+
+    //     console.log(e.target.value.split(/\r?\n/));
+    // }
+
+    // const areaSelected = (e) => {
+    //     alert(window.getSelection())
+    // }
+
+    // const handleChecked = (e) => {
+    //     setState({
+    //         ...state,
+    //         textOrList: e.target.checked,
+    //     })
+    // }
 
     // <div className="resp-form">
     //     <p>Add responsibilities</p>
@@ -176,46 +202,51 @@ export default function Experience() {
     // </div >
 
     // {(e) => setState({...state, experiencebody: e.target.value})} 
+    /* <input type="submit" name="expForm" id="exp-submit-btn" /> */
+
     if (state.addExp) {
-        listOrTextSwitch = toggleBetweenListOrText(state, listOrTextSwitch);
+        // listOrTextSwitch = toggleBetweenListOrText(state, listOrTextSwitch);
 
         addExperienceWindow = (
             <div className="add-experience-container">
                 <div className="add-experience-window">
-                    <p className="add-experience">Add Experience</p>
-                    <form onSubmit={handleOnSave} htmlFor="expForm" action="">
-                        <label htmlFor="">
-                            <p>Job Title</p>
-                            <input required onChange={handleOnChange} type="text" name="expForm" id="jobTitle" />
-                        </label>
-                        <label htmlFor="">
-                            <p>Employer</p>
-                            <input onChange={handleOnChange} type="text" name="employer" id="employer" />
-                        </label>
-                        <label htmlFor="">
-                            <p>Country</p>
-                            <input onChange={handleOnChange} type="text" name="jobCountry" id="jobCountry" />
-                        </label>
-                        <label htmlFor="">
-                            <p>City</p>
-                            <input onChange={handleOnChange} type="text" name="jobCity" id="jobCity" />
-                        </label>
-                        <label htmlFor="">
-                            <p>Start date</p>
-                            <input onChange={handleOnChange} type="date" name="jobStartDate" id="startDate" />
-                        </label>
-                        <label htmlFor="">
-                            <p>End date</p>
-                            <input onChange={handleOnChange} type="date" name="jobEndDate" id="endDate" />
-                        </label>
-
-                    </form>
-
-
-                    <div className="add-experience-btn-controls" htmlFor="">
-                        <button>cancel</button>
-                        <input type="submit" name="expForm" id="exp-submit-btn" />
+                    <div style={{ left: styleData.jobInfoLeft }} id="jobInfoLeft" className="slide add-job-info">
+                        <p className="add-experience">Add Experience</p>
+                        <form onSubmit={handleOnSave} htmlFor="expForm" action="">
+                            <label htmlFor="">
+                                <p>Job Title</p>
+                                <input required onChange={handleOnChange} type="text" name="expForm" id="jobTitle" />
+                            </label>
+                            <label htmlFor="">
+                                <p>Employer</p>
+                                <input onChange={handleOnChange} type="text" name="employer" id="employer" />
+                            </label>
+                            <label htmlFor="">
+                                <p>Country</p>
+                                <input onChange={handleOnChange} type="text" name="jobCountry" id="jobCountry" />
+                            </label>
+                            <label htmlFor="">
+                                <p>City</p>
+                                <input onChange={handleOnChange} type="text" name="jobCity" id="jobCity" />
+                            </label>
+                            <label htmlFor="">
+                                <p>Start date</p>
+                                <input onChange={handleOnChange} type="date" name="jobStartDate" id="startDate" />
+                            </label>
+                            <label htmlFor="">
+                                <p>End date</p>
+                                <input onChange={handleOnChange} type="date" name="jobEndDate" id="endDate" />
+                            </label>
+                        </form>
                     </div>
+                    <div style={{ left: styleData.jobResponsbLeft }} id="jobResponsbLeft" className="slide add-job-responsibilities">
+                        <textarea name="responsb" id="responsb" cols="30" rows="10">
+                        </textarea>
+                    </div>
+                </div>
+                <div className="add-experience-btn-controls" htmlFor="">
+                    <button onClick={handleCancelAddingExperience}>cancel</button>
+                    <button onClick={handleContinue}>continue</button>
                 </div>
             </div>)
     }
@@ -223,7 +254,6 @@ export default function Experience() {
     if (state.input) {
         experienceViewToggle =
             <div className="edit-page">
-                {addExperienceWindow}
                 <aside>
                     <header><p>CV Maker</p></header>
                 </aside>
@@ -266,6 +296,8 @@ export default function Experience() {
                     <button onClick={handleBack}>Back</button>
                     <button onClick={handleAddingExperience}>Add more experience</button>
                 </div>
+                {addExperienceWindow}
+
             </div>
     } else {
         experienceViewToggle =
